3e6b34508183d2b7b8a0a82869e62f4c659af684
Yuliya Maximchikova <m.yuliyas.00@gmail.com>
2019-10-21 23:32:42 +0300

    headers, form-parameter, json, xml

--- server_module.js
+++ server_module.js
@@ -1,5 +1,10 @@
 const http = require('http');
 const url = require('url');
+const fs = require('fs');
+const mp = require('multiparty');
+const Stat = require('D://Study/PSCP/lr_7/m07-01')('D://Study/PSCP/lr_8/');
+
+const dir = './files/';
 
 let calc = (x, y, req, res) => {
     res.writeHead(200, {'Content-Type':'text/plain; charset=utf-8'});
@@ -29,13 +34,16 @@ let get_handler = (req, res) => {
     switch(true) {
         case path === '/connection':
             res.writeHead(200, {'Content-Type':'text/plain; charset=utf-8'});
-            res.end(`KeepAliveTimeout: ${server.keepAliveTimeout}`);
-            break;
-        case /\/connection\/set=\d+/.test(path):
-            server.keepAliveTimeout = parseInt(/\d+/.exec(path)[0], 10);
+            let url_parts = url.parse(req.url, true);
+            let query = url_parts.query;
+            let set = parseInt(query.set, 10);
+            if (!isNaN(set)) {
+                server.keepAliveTimeout = set;
+            }
             res.end(`KeepAliveTimeout: ${server.keepAliveTimeout}`);
             break;
         case path === '/headers':
+            res.setHeader('My_Header', 'true');
             res.writeHead(200, {'Content-Type':'application/json; charset=utf-8'});
             res.write(`REQUEST HEADERS: ${JSON.stringify(req.headers)}\n`);
             res.write(`RESPONSE HEADERS: ${JSON.stringify(res.headers)}\n`);
@@ -53,22 +61,23 @@ let get_handler = (req, res) => {
             }
             break;
         }
-        case path === '/parameter':
+        case path === '/parameter': {
             let url_parts = url.parse(req.url, true);
             let query = url_parts.query;
             let x = parseInt(query.x, 10);
             let y = parseInt(query.y, 10);
-            if(isNaN(x) || isNaN(y)) {
-                res.writeHead(405, {'Content-Type':'text/plain; charset=utf-8'});
+            if (isNaN(x) || isNaN(y)) {
+                res.writeHead(405, {'Content-Type': 'text/plain; charset=utf-8'});
                 res.end('ERROR 405: x and y must be numbers');
             } else {
                 calc(x, y, req, res);
             }
             break;
+        }
         case path === '/close':
             res.writeHead(200, {'Content-Type':'text/plain; charset=utf-8'});
             res.end("Server will be closed in 10 sec.");
-            server.setTimeout(10000, () => server.close(() => console.log('Server closed.')));
+            let timer = setTimeout(() => {server.close(() => {console.log('Server closed.');});}, 10000);
             break;
         case path === '/socket':
             res.writeHead(200, {'Content-Type':'text/plain; charset=utf-8'});
@@ -88,11 +97,61 @@ let get_handler = (req, res) => {
             req.on('end', () => {console.log(`Data length(end): ${buf.length}`);});
             res.end();
             break;
+        case path === '/resp-status': {
+            let url_parts = url.parse(req.url, true);
+            let query = url_parts.query;
+            let code = parseInt(query.code, 10);
+            let mess = query.mess;
+            res.writeHead(200, mess, {'Content-Type': 'text/plain; charset=utf-8'});
+            res.end(`Status code: ${code}\nStatus message: ${mess}`);
+            break;
+        }
+        case path === '/files':
+            fs.readdir(dir, (err, files) => {
+                res.writeHead(200, {'Content-Type':'text/plain; charset=utf-8', 'X-static-files-count': files.length});
+                res.end(`Files count: ${files.length}`);
+            });
+            break;
+        case /\/files\/.+/.test(path): {
+            let arr = path.split('/');
+            let file_path = arr[2];
+            fs.readFile(dir + file_path, (err, data) => {
+               if (err) {
+                   res.writeHead(404, {'Content-Type':'text/plain; charset=utf-8'});
+                   res.end(`ERROR 404: ${file_path} is not found.`);
+               } else {
+                   Stat.sendFile(req, res);
+               }
+            });
+            break;
+        }
+        case path === '/upload':
+            fs.readFile(dir + 'webform.html', (err, data) => {
+                res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
+                res.end(data);
+            });
+            break;
     }
 };
 
 let post_handler = (req, res) => {
-
+    let path = url.parse(req.url).pathname;
+    switch(true) {
+        case path === '/upload':
+            let form = new mp.Form({uploadDir: './files'});
+            form.on('file', (name, file) => {
+               fs.copyFile(name, form.uploadDir, () => {console.log('File is copied.');});
+            });
+            form.parse(req);
+            let result = '';
+            req.on('data', (data) => {result += data;});
+            req.on('end', () => {
+                res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
+                res.write('<h1>File upload</h1>');
+                res.end(result);
+            });
+            break;
+    }
 };
 
 let server = http.createServer();
